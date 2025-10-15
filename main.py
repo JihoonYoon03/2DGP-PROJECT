e0294a50bb80947d61d033f74d93d02048264e21
Yoon Ji Hoon <wlgns7922@gmail.com>
2025-10-16 00:40:19 +0900

    RoboSpider 클래스 추가

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from pico2d import *
 
 open_canvas(800, 600)
 
+class RoboSpider:
+    def __init__(self):
+        self.x = 400
+        self.y = 300
+        self.frame = 0
+        # self.image = load_image('robo_spider.png')
+
 while True:
     clear_canvas()
 
