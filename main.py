90251cb2171f0ac55c04c0bd1a67633d983dc783
Yoon Ji Hoon <wlgns7922@gmail.com>
2025-10-17 00:25:48 +0900

    spider moving 애니메이션 출력 테스트

--- main.py
+++ main.py
@@ -8,16 +8,53 @@ class RoboSpider:
         self.x = 400
         self.y = 300
         self.frame = 0
-        # self.image = load_image('robo_spider.png')
+        self.image = load_image('Assets/Sprites/Spider/Spider_Moving.png')
+        self.width = self.image.w
+        self.height = self.image.h
+
+    def update(self):
+        self.frame = (self.frame + 1) % 16
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 178 % (11 * 178), self.height - 440 - (self.frame // 11 * 444), 178, 440, self.x, self.y)
+        print(f'{self.frame // 10}')
+
+
+def reset_world():
+    global world
+
+    world = []
+    spider = RoboSpider()
+    world.append(spider)
+
+def handle_events():
+    pass
+
+
+def update_world():
+    global world
+    for thing in world:
+        thing.update()
+
+
+def render_world():
+    global world
+
+    for thing in world:
+        thing.draw()
+
+reset_world()
 
 while True:
     clear_canvas()
 
-    # render
-
-
     # logic
+    handle_events()
+    update_world()
 
+    # render
+    render_world()
+    update_canvas()
     delay(0.05)
 
 close_canvas()
