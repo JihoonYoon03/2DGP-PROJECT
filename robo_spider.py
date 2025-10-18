efdb0b3b43388738c9e31167d61b5d94695d4582
Yoon Ji Hoon <wlgns7922@gmail.com>
2025-10-19 02:08:12 +0900

    IDLE 상태 추가 및 출력 로직 완성

--- robo_spider.py
+++ robo_spider.py
@@ -3,6 +3,22 @@ from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK
 
 from state_machine import StateMachine
 
+class Idle:
+    def __init__(self, spider):
+        self.spider = spider
+
+    def enter(self, e):
+        pass
+
+    def exit(self, e):
+        pass
+
+    def do(self):
+        pass
+
+    def draw(self):
+        self.spider.image.clip_draw(self.spider.frame * 178 % (11 * 178), self.spider.height - 440 - (self.spider.frame // 11 * 444), 178, 440, self.spider.x, self.spider.y)
+
 class RoboSpider:
     def __init__(self):
         self.x = 400
@@ -12,12 +28,15 @@ class RoboSpider:
         self.width = self.image.w
         self.height = self.image.h
 
-        self.stateMachine = StateMachine()
+        self.IDLE = Idle(self)
+        self.stateMachine = StateMachine(
+            self.IDLE,
+        {
+            self.IDLE : {}
+        })
 
     def update(self):
         self.stateMachine.update()
-        #self.frame = (self.frame + 1) % 16
 
     def draw(self):
         self.stateMachine.draw()
-        #self.image.clip_draw(self.frame * 178 % (11 * 178), self.height - 440 - (self.frame // 11 * 444), 178, 440, self.x, self.y)
--- state_machine.py
+++ state_machine.py
@@ -1,12 +1,22 @@
 class StateMachine:
-    def __init__(self):
-        pass
+    def __init__(self, cur_state, rules):
+        self.cur_state = cur_state
+        self.next_state = None
+        self.rules = rules
+        self.cur_state.enter(('START', None))
 
     def update(self):
-        pass
+        self.cur_state.do()
 
-    def handle_state_event(self):
-        pass
+    def handle_state_event(self, state_event):
+        for check_event in self.rules[self.cur_state].keys():
+            if check_event(state_event):
+                self.next_state = self.rules[self.cur_state][check_event]
+                self.cur_state.exit(state_event)
+                self.next_state.enter(state_event)
+                # print(f'{self.cur_state.__class__.__name__} - {state_event[0]} -> {self.next_state.__class__.__name__}')
+                self.cur_state = self.next_state
+                return
 
     def draw(self):
-        pass
+        self.cur_state.draw()
