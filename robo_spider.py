7e2cc534e7b2923daa75e7ac40e4311494323dd8
laptop-msi <wlgns7922@gmail.com>
2025-10-20 11:51:57 +0900

    아래 움직임 상태 추가 및 출력 사이즈 조정

--- robo_spider.py
+++ robo_spider.py
@@ -3,15 +3,24 @@ from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK
 
 from state_machine import StateMachine
 
+SizeOffset = 0.4
+
 def w_pressed(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_w
 
 def w_released(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_w
 
+def s_pressed(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_s
+
+def s_released(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_s
+
 class SpIdle:
     def __init__(self, spider):
         self.spider = spider
+        self.size = spider.size
 
     def enter(self, e):
         pass
@@ -23,11 +32,15 @@ class SpIdle:
         pass
 
     def draw(self):
-        self.spider.image.clip_draw(self.spider.frame * 178 % (11 * 178), self.spider.height - 440 - (self.spider.frame // 11 * 444), 178, 440, self.spider.x, self.spider.y)
+        self.spider.image.clip_draw(self.spider.frame * 178 % (11 * 178), self.spider.height - 440 - (self.spider.frame // 11 * 444), 178, 440,
+                                    self.spider.x, self.spider.y, 178 * self.size, 440 * self.size)
 
 class SpUp:
     def __init__(self, spider):
         self.spider = spider
+        self.size = spider.size
+        self.w = spider.image.w
+        self.h = spider.image.h
 
     def enter(self, e):
         pass
@@ -38,13 +51,38 @@ class SpUp:
 
     def do(self):
         self.spider.frame = (self.spider.frame + 1) % 16
-        self.spider.y += 5
+        self.spider.y += 5 * self.size
 
     def draw(self):
-        self.spider.image.clip_draw(self.spider.frame * 178 % (11 * 178), self.spider.height - 440 - (self.spider.frame // 11 * 444), 178, 440, self.spider.x, self.spider.y)
+        self.spider.image.clip_draw(self.spider.frame * 178 % (11 * 178), self.spider.height - 440 - (self.spider.frame // 11 * 444), 178, 440,
+                                    self.spider.x, self.spider.y, 178 * self.size, 440 * self.size)
+
+class SpDown:
+    def __init__(self, spider):
+        self.spider = spider
+        self.size = spider.size
+        self.w = spider.image.w
+        self.h = spider.image.h
+
+    def enter(self, e):
+        pass
+
+    def exit(self, e):
+        # 추가 구현 필요사항: 애니메이션이 끝나기 전엔 IDLE 상태로 넘어가면 안됨.
+        pass
+
+    def do(self):
+        self.spider.frame = self.spider.frame - 1 if self.spider.frame > 0 else 15
+        self.spider.y -= 5 * self.size
+
+    def draw(self):
+        self.spider.image.clip_draw(self.spider.frame * 178 % (11 * 178), self.spider.height - 440 - (self.spider.frame // 11 * 444), 178, 440,
+                                    self.spider.x, self.spider.y, 178 * self.size, 440 * self.size)
 
 class RoboSpider:
     def __init__(self):
+        global SizeOffset
+        self.size = SizeOffset
         self.x = 400
         self.y = 300
         self.frame = 0
@@ -53,12 +91,14 @@ class RoboSpider:
         self.height = self.image.h
 
         self.IDLE = SpIdle(self)
-        self.MOVEUP = SpUp(self)
+        self.UP = SpUp(self)
+        self.DOWN = SpDown(self)
         self.stateMachine = StateMachine(
             self.IDLE,
         {
-            self.IDLE : { w_pressed : self.MOVEUP },
-            self.MOVEUP : { w_released : self.IDLE }
+            self.IDLE : { w_pressed : self.UP, s_pressed : self.DOWN },
+            self.UP : { w_released : self.IDLE, s_pressed : self.DOWN },
+            self.DOWN : { s_released : self.IDLE, w_pressed : self.UP }
         })
 
     def update(self):
