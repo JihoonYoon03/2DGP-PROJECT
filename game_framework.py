4b5ec66d46bff1ef69cab33cfd97b364f977fefe
laptop-msi <wlgns7922@gmail.com>
2025-11-06 13:24:23 +0900

    시간 개념 추가

--- game_framework.py
+++ game_framework.py
@@ -1,6 +1,9 @@
+import time
+
 running = None
 stack = None
 
+frame_time = 0.0
 
 def change_scene(scene):
     global stack
@@ -36,14 +39,23 @@ def quit():
 
 def run(start_scene):
     global running, stack
+    global frame_time
+
     running = True
     stack = [start_scene]
     start_scene.init()
 
+    frame_time = 0.0
+    start_time = time.time()
     while running:
+
         stack[-1].handle_events()
         stack[-1].update()
         stack[-1].draw()
+
+        frame_time = time.time() - start_time
+        start_time += frame_time
+        frame_rate = 1.0 / frame_time
 
     while (len(stack) > 0):
         stack[-1].finish()
