493851c43111b6a22340993b501f998711a4a94b
Yoon Ji Hoon <wlgns7922@gmail.com>
2025-11-08 01:05:03 +0900

    mine_data 초안 작성

---
+++ mine_data.py
@@ -0,0 +1,13 @@
+from tile import TileFlag
+
+data_set = {
+    {1: (
+        # 타일 이미지
+        {'image': 'Assets/Sprites/Tile/Block01Tileset.png'},
+
+        # 타일 배치 및 플래그.
+        {'small': ({'location': False},
+                   {'flag': False}
+        )}
+    )}
+}
