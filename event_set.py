cb2443e1e45960a63763444115aef0d500358650
Yoon Ji Hoon <wlgns7922@gmail.com>
2025-11-08 21:30:11 +0900

    player 상태 및 키 입력 함수 추가

--- event_set.py
+++ event_set.py
@@ -1,20 +1,42 @@
-from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_a, SDLK_w, SDLK_s, SDLK_r
+import sdl2
 from camera import Camera
 
 def time_out(e):
     return e[0] == 'TIME_OUT'
 
+def e_pressed(e):
+    return e[0] == 'INPUT' and e[1].type == sdl2.SDL_KEYDOWN and e[1].key == sdl2.SDLK_e
+
+def e_released(e):
+    return e[0] == 'INPUT' and e[1].type == sdl2.SDL_KEYUP and e[1].key == sdl2.SDLK_e
+
 def w_pressed(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_w
+    return e[0] == 'INPUT' and e[1].type == sdl2.SDL_KEYDOWN and e[1].key == sdl2.SDLK_w
 
 def w_released(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_w
+    return e[0] == 'INPUT' and e[1].type == sdl2.SDL_KEYUP and e[1].key == sdl2.SDLK_w
+
+def a_pressed(e):
+    return e[0] == 'INPUT' and e[1].type == sdl2.SDL_KEYDOWN and e[1].key == sdl2.SDLK_a
+
+def a_released(e):
+    return e[0] == 'INPUT' and e[1].type == sdl2.SDL_KEYUP and e[1].key == sdl2.SDLK_a
 
 def s_pressed(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_s
+    return e[0] == 'INPUT' and e[1].type == sdl2.SDL_KEYDOWN and e[1].key == sdl2.SDLK_s
 
 def s_released(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_s
+    return e[0] == 'INPUT' and e[1].type == sdl2.SDL_KEYUP and e[1].key == sdl2.SDLK_s
+
+def d_pressed(e):
+    return e[0] == 'INPUT' and e[1].type == sdl2.SDL_KEYDOWN and e[1].key == sdl2.SDLK_d
+
+def d_released(e):
+    return e[0] == 'INPUT' and e[1].type == sdl2.SDL_KEYUP and e[1].key == sdl2.SDLK_d
 
 def r_pressed(e):
-    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_r
+    return e[0] == 'INPUT' and e[1].type == sdl2.SDL_KEYDOWN and e[1].key == sdl2.SDLK_r
+
+def no_key_pressed(e):
+    return e[0] == 'INPUT' and e[1].type == sdl2.SDL_KEYDOWN and \
+        e[1].key not in (sdl2.SDLK_w, sdl2.SDLK_a, sdl2.SDLK_s, sdl2.SDLK_d)
--- player.py
+++ player.py
@@ -1,7 +1,58 @@
 from pico2d import *
 from event_set import *
 from game_world import get_camera
+from state_machine import StateMachine
 
+class Docked:
+    def __init__(self, sp):
+        self.sp = sp
+
+    def enter(self, e):
+        pass
+
+    def exit(self, e):
+        return True
+
+    def do(self):
+        pass
+
+    def draw(self):
+        camera = get_camera()
+        pass
+
+class Idle:
+    def __init__(self, sp):
+        self.sp = sp
+
+    def enter(self, e):
+        pass
+
+    def exit(self, e):
+        return True
+
+    def do(self):
+        pass
+
+    def draw(self):
+        camera = get_camera()
+        pass
+
+class Move:
+    def __init__(self, sp):
+        self.sp = sp
+
+    def enter(self, e):
+        pass
+
+    def exit(self, e):
+        return True
+
+    def do(self):
+        pass
+
+    def draw(self):
+        camera = get_camera()
+        pass
 
 class Player:
     def __init__(self, robo_spider):
@@ -10,12 +61,25 @@ class Player:
         self.image_move_up = load_image('Assets/Sprites/Player/Hero_Up_Moving.png')
         self.image_move_down = load_image('Assets/Sprites/Player/Hero_Down_Moving.png')
 
-        self.is_docked = True
+        self.is_docked = True   # 스파이더에 도킹 여부
         self.x = robo_spider.x
         self.y = robo_spider.y
         self.face_dir = 0 # 1: right, -1: left, 2: up, -2: down
         self.move_x = 0
         self.move_y = 0
+
+        self.DOCKED = Docked(self)
+        self.IDLE = Idle(self)
+        self.MOVE = Move(self)
+
+        self.stateMachine = StateMachine(
+            self.DOCKED,
+            {
+                self.DOCKED: { e_pressed : self.IDLE },
+                self.IDLE: { w_pressed : self.MOVE, a_pressed : self.MOVE, s_pressed : self.MOVE, d_pressed : self.MOVE },
+                self.MOVE: { w_released : self.MOVE, a_released : self.MOVE, s_released : self.MOVE, d_released : self.MOVE,
+                             no_key_pressed : self.IDLE }
+            })
 
         self.robo_spider = robo_spider
 
